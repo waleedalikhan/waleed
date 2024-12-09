2cfccce4e0647868801f2585d790c916dcadf80b
Maratib Ali Khan <maratib@gmail.com>
2024-12-09 23:55:57 +0500

    initial commit

---
+++ src/app/app.config.ts
@@ -0,0 +1,23 @@
+import {
+  provideHttpClient,
+  withFetch,
+  withInterceptors,
+} from '@angular/common/http';
+import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { provideClientHydration } from '@angular/platform-browser';
+import { provideFileRouter, requestContextInterceptor } from '@analogjs/router';
+import { provideContent, withMarkdownRenderer } from '@analogjs/content';
+import { withShikiHighlighter } from '@analogjs/content/shiki-highlighter';
+
+export const appConfig: ApplicationConfig = {
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideFileRouter(),
+    provideHttpClient(
+      withFetch(),
+      withInterceptors([requestContextInterceptor])
+    ),
+    provideClientHydration(),
+    provideContent(withMarkdownRenderer(), withShikiHighlighter()),
+  ],
+};
